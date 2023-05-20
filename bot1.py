9de7e2309252a045fd4029b7f68fd16f3844cf25
rs0125 <92998665+rs0125@users.noreply.github.com>
2023-05-20 22:40:15 +0530

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,7 +1,6 @@
 import discord
 
-TOKEN = 'xxxx'
-print('hello')
+TOKEN = 'xxxxxxxxxxxxxx'
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,20 +11,23 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+subjects = ">>> Available subs:\n1. BEEE \n2. Engineering Calculus \n3. Chemistry \n "
 
 @client.event
 async def on_message(message):
+
+    if message.content.startswith('!hi'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('!subs'):
+        sub = await message.channel.send(subjects)
+        await sub.add_reaction("1️⃣")
+        await sub.add_reaction("2️⃣")
+        await sub.add_reaction("3️⃣")
+        await sub.add_reaction("4️⃣")
+
+
     if message.author == client.user:
         return
-
-    if message.content.startswith('!hi'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('!hi'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('!hi'):
-        await message.channel.send('Hello!')
-
 
 client.run(TOKEN)
