41a5f2c14d16c56fea980b04160ee3bf8331fd95
rs0125 <92998665+rs0125@users.noreply.github.com>
2023-05-22 00:04:10 +0530

    Update bot2_ButtonUI.py
    Added slash commands, bugfixing

--- bot2_ButtonUI.py
+++ bot2_ButtonUI.py
@@ -1,20 +1,10 @@
 import discord
 from discord.ui import Button, View
 from discord import app_commands
-from discord.ext import commands
-TOKEN = "XXXXXXXXXX"
-
+TOKEN = "xxxxxxxxxxxxxx"
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-
-
-@client.event
-async def on_ready():
-    print("Link established")
-
 
 #UI for all subs
 
@@ -27,27 +17,19 @@ ENPH = Button(label="Engineering Physics", style=discord.ButtonStyle.blurple)
 
 CALC = Button(label="Calculus", style=discord.ButtonStyle.blurple)
 
-#interaction
 @client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith("!sub"):
-        view = View()
-        view.add_item(BEEE)
-        view.add_item(CHEM)
-        view.add_item(ENPH)
-        view.add_item(CALC)
-
-        async def button_callback(interaction):
-            viewBEEE = View()
-            viewBEEE.add_item(BEEE_link1)
-            await interaction.channel.send('Available PDFs are :', view = viewBEEE)
-            await interaction.response.defer()
-        BEEE.callback =button_callback
-
-        await message.channel.send('Available subjects are :', view = view)
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=1109016227210350634))
+    print("Edulink established!")
 
 
+@tree.command(name = "subjects", description = "displays available subjects", guild=discord.Object(id=1109016227210350634))
+async def first_command(interaction,):
+    view = View()
+    view.add_item(BEEE)
+    view.add_item(CHEM)
+    view.add_item(ENPH)
+    view.add_item(CALC)
+    await interaction.response.send_message('Available subjects are :', view=view)
 
 client.run(TOKEN)
