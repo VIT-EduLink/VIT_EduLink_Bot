daceae209d145c2783f2a21340d7c89f2c884aae
puang59 <kk61468@gmail.com>
2023-05-29 02:55:47 +0530

    Added connection with express api and added dynamic buttons

--- archive/views/buttons.py
+++ archive/views/buttons.py
@@ -1,10 +1,20 @@
 import discord 
 from discord import app_commands 
 from discord.ext import commands 
+import asyncpg 
 
 class Subjects(discord.ui.View): 
     """ This class contains all the subject button views """
+    def __init__(self, bot):
+        self.bot = bot
+        self.database = None
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Buttons loaded!')
+        # Establish database connection
+        self.database = await asyncpg.create_pool(database='vitedulink', user='puang', password='61468')
+ 
     @discord.ui.button(label='BEEE', style=discord.ButtonStyle.blurple)
     async def BEEE(self, interaction: discord.Interaction, button: discord.ui.Button):
         resource = Resources()
@@ -16,11 +26,13 @@ class Subjects(discord.ui.View):
 
     @discord.ui.button(label='Chemistry', style=discord.ButtonStyle.blurple)
     async def CHEM(self, interaction: discord.Interaction, button: discord.ui.Button):
-        resource = Resources()
-        resource.add_item(resource.CHEM_link1)
-        resource.add_item(resource.CHEM_link2)
-        resource.add_item(resource.CHEM_link3)
-        resource.add_item(resource.CHEM_link4)
+        query = 'SELECT * FROM chemistry;'
+        links = await self.database.fetch(query)
+        resource = Resources(links)
+        # resource.add_item(resource.CHEM_link1)
+        # resource.add_item(resource.CHEM_link2)
+        # resource.add_item(resource.CHEM_link3)
+        # resource.add_item(resource.CHEM_link4)
         await interaction.response.send_message("`[CHEMISTRY]` Available PDFs are: ", view=resource)
         
     @discord.ui.button(label='Engineering Physics', style=discord.ButtonStyle.blurple)
@@ -42,6 +54,10 @@ class Subjects(discord.ui.View):
         await interaction.response.send_message("`[CALCULUS]` Available PDFs are: ", view=resource)
 
 class Resources(discord.ui.View):
+    def __init__(self, links):
+        super().__init__()
+        for link in links: 
+            self.add_item(discord.ui.Button(label=link['label'], style=discord.ButtonStyle.link, url=link['url']))
     """ This class contains all the pdf links (for now its rickroll but in future it will be fetching requests from backend API) """
     #BEEE Links
     BEEE_link1= discord.ui.Button(label='Link1', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
@@ -50,10 +66,10 @@ class Resources(discord.ui.View):
     BEEE_link4= discord.ui.Button(label='Link4', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
     #CHEM Links
-    CHEM_link1= discord.ui.Button(label='Link1', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    CHEM_link2= discord.ui.Button(label='Link2', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    CHEM_link3= discord.ui.Button(label='Link3', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    CHEM_link4= discord.ui.Button(label='Link4', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    # CHEM_link1= discord.ui.Button(label='Link1', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    # CHEM_link2= discord.ui.Button(label='Link2', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    # CHEM_link3= discord.ui.Button(label='Link3', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    # CHEM_link4= discord.ui.Button(label='Link4', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
     #ENPH Links
     ENPH_link1= discord.ui.Button(label='Link1', style=discord.ButtonStyle.link, url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
